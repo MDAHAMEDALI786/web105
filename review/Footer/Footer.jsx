2c5deae79aa191739351a24184da99125504df30
MDAHAMEDALI <88301246+MDAHAMEDALI786@users.noreply.github.com>
2021-12-27 08:39:48 +0530

    Add files via upload

---
+++ review/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+import React from "react";
+
+function template() {
+  return (
+    <div className="footer">
+&copy; rights belongs to 𝕄𝕆ℕ𝕊𝕋𝔼ℝ    </div>
+  );
+};
+
+export default template;
